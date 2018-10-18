6b9cbc5de26924af543a1065ffa185d56cf1ef75
Weasdf <mincubill@gmail.com>
2018-10-18 18:16:31 -0300

    Initial checkin

---
+++ client/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name arkhamHelperApp
+ * @description
+ * # arkhamHelperApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('arkhamHelperApp', []);
